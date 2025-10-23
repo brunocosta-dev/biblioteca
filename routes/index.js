a2c0b754ddd6b98af59a6fff43c361d91d19026f
Bruno Costa <bruno.costa.dev@outlook.com>
2025-10-22 22:22:03 -0300

    Adicionado codigo do html e logiga das rotas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+    {
+      title: "Sistema de Biblioteca",
+      erro: "Usuário/Senha Inválidos"
+    })
+});
+
+router.post('/login', function(req,res,next){
+  const {usuario, senha} = req.body;
+  if(usuario === 'tiaogaviao@gmail.com' && senha === '123'){
+    res.send("Deu certo! Faz de conta que você está no dashboard");
+  }
+  else{
+    if(usuario !== "tiaogaviao@gmail.com"){
+      res.send("Usuário Inválido")
+    }else{
+      res.send("Senha Inválida!")
+    }
+    
+  }
+
+
+  res.send('Vou ver se o usuario/senha existe - aguarde...')
+});
+
+router.get('/listagem', function (req,res, next){
+  res.send('Aqui vai aparece a lista de livros!')
 });
 
 module.exports = router;
